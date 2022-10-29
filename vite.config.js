6d6e9513258c8759ae7e68d1cf9ab06f0dd45699
ztn_home <zhutiannian15@mails.ucas.ac.cn>
2022-10-29 12:47:53 +0800

    221029-1

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: `http://0.0.0.0:4000/api`,
+        target: 'http://127.0.0.1:4000/api',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
